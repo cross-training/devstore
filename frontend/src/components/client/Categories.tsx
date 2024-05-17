faed651fd4500f09866e66aaecd7def8e6b0929a
Jean Paul Val <jp@jpval.me>
2024-05-17 19:02:08 +0200

    feat: initial client-page layout
    #1

---
+++ frontend/src/components/client/Categories.tsx
@@ -0,0 +1,11 @@
+function Categories() {
+    return (
+        <>
+            <div className='w-52 flex bg-slate-500'>
+                    
+            </div>
+        </>
+    )
+}
+
+export default Categories;
